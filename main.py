e5cf434c13598c1930661841d2aaa0d8d5da2683
fiona1994murray <82724652+fiona1994murray@users.noreply.github.com>
2021-04-18 21:08:43 +0100

    Commit CSV

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Any, Union
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas import DataFrame, Series
+from pandas.io.parsers import TextFileReader
+
+WHR = pd.read_csv("/Applications/WHR/world-happiness-report.numbers")
+
+print(WHR.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
